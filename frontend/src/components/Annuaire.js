68dbcfbd733a0a1b1e6671379f03012dd8dd5f30
nicolasmarro <nicolas.marro@he-arc.ch>
2022-05-06 01:59:03 +0200

    Amélioration graphique

--- frontend/src/components/Annuaire.js
+++ frontend/src/components/Annuaire.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import axios from 'axios';
-import { ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/annuaire/notaires'
-  })
+})
 
 class Notaires extends React.Component {
 
@@ -20,13 +20,27 @@ class Notaires extends React.Component {
         })
     }
 
-  render() {
-      return(
-        <ListGroup>
-            { this.state.notaires.map(notaire => <ListGroup.Item>{notaire.nom} {notaire.prenom}</ListGroup.Item>)}
-      </ListGroup>
-      )
-  }
+    render() {
+        return (
+            this.state.notaires.map(notaire => {
+                console.log(notaire);
+                return (
+                    <Container >
+                    <Row className="justify-content-md-center">
+                            <Col md={4}>
+                                <Card border='dark' style={{ width: '18rem' }} className="mx-auto my-2">
+                                    <Card.Body>
+                                        <Card.Title>{notaire.nom}</Card.Title>
+                                        <Card.Subtitle className="mb-2 text-muted">{notaire.prenom}</Card.Subtitle>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                    </Row>
+                    </Container>
+                )
+            })
+        )
+    }
 }
 
 export default Notaires
