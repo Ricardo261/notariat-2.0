db30b6430d65719d84615f9520ac320879769d62
nicolasmarro <nicolas.marro@he-arc.ch>
2022-05-05 16:29:41 +0200

    Composant Notaires

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ButtonAppBar from './components/ButtonAppBar';
+import Notaires from './components/Notaires';
 
 
 function App() {
   return (
     <div className='App'>
       
-        <ButtonAppBar></ButtonAppBar>
+      <ButtonAppBar></ButtonAppBar>
+      <Notaires></Notaires>
     
     </div>
   );
--- frontend/src/components/Notaires.js
+++ frontend/src/components/Notaires.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-function Notaires() {
-    return (
-      <div className='App'>
-        
-          <ButtonAppBar></ButtonAppBar>
-      
-      </div>
-    );
-  }
+const axios = require('axios').default;
+
+export default class OutlinedCard extends React.Component {
+    state = {
+        persons: []
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/annuaire/notaires')
+        .then(function (response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .then(res => {
+            const persons = res.data;
+            this.setState({ persons });
+          })
+    }
   
-  export default Notaires;
+    render() {
+        return (
+          <ul>
+            { this.state.persons.map(person => <li>{person.name}</li>)}
+          </ul>
+        )
+    }
+}
