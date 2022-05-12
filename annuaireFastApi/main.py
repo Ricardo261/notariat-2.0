3c41b3286177ef9bb9425ef01fff152d19d34ebe
leonardfrot <leonard.frote@he-arc.ch>
2022-05-12 15:51:15 +0200

    correction authentification

--- annuaireFastApi/main.py
+++ annuaireFastApi/main.py
@@ -5,7 +5,6 @@ import requests
 import json
 import logging
 
-
 app = FastAPI()
 
 origins = [
@@ -23,10 +22,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class User(BaseModel):
-    login: str
-    password: str
-
 
 @app.get("/")
 async def root():
@@ -43,66 +38,37 @@ async def getNotaire(prenom: str):
     nom = getNotaires().get(prenom.title())
     return (prenom.title() + " " + nom) if nom != None else "Notaire inconnu"
 
-@app.post("/api/login")
-async def login(user: User):
-    logging.info(user)
-    url = "http://localhost:8080/api/login"
-
-    payload = json.dumps({
-        "login": user.login,
-        "password": user.password
-    })
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    return requests.request("POST", url, headers=headers, data=payload).json()
-
-@app.post("/api/signup")
-async def login(user: User):
-    logging.info(user)
-    url = "http://localhost:8080/api/signup"
-
-    payload = json.dumps({
-        "login": user.login,
-        "password": user.password
-    })
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    return requests.request("POST", url, headers=headers, data=payload).json()
 
 def getNotaires():
-    return  [
-                { 
-                    "nom" : "AUGSBURGER", 
-                    "prenom" : "Isabelle",
-                    "adresse" : "Rue du Musée 6",
-                    "localite" : "2000 Neuchâtel"
-                },
-                {
-                    "nom" : "BLANDENIER",
-                    "prenom" : "Christian",
-                    "adresse" : "Impasse du Noyer 1",
-                    "localite" : "2053 Cernier"
-                },
-                {
-                    "nom" : "FELD",
-                    "prenom" : "Laurent",
-                    "adresse" : "Place de la Fontaine 4",
-                    "localite" : "2034 Peseux"
-                },
-                {
-                    "nom" : "GUILLEN",
-                    "prenom" : "Lila",
-                    "adresse" : "Rue Denis-de-Rougemont 3",
-                    "localite" : "2108 Couvet"
-                },
-                {
-                    "nom" : "OBRIST",
-                    "prenom" : "Laurent",
-                    "adresse" : "Rue de l'Hôpital 11",
-                    "localite" : "2001 Neuchâtel"
-                }
-            ]
+    return [
+        {
+            "nom": "AUGSBURGER",
+            "prenom": "Isabelle",
+            "adresse": "Rue du Musée 6",
+            "localite": "2000 Neuchâtel"
+        },
+        {
+            "nom": "BLANDENIER",
+            "prenom": "Christian",
+            "adresse": "Impasse du Noyer 1",
+            "localite": "2053 Cernier"
+        },
+        {
+            "nom": "FELD",
+            "prenom": "Laurent",
+            "adresse": "Place de la Fontaine 4",
+            "localite": "2034 Peseux"
+        },
+        {
+            "nom": "GUILLEN",
+            "prenom": "Lila",
+            "adresse": "Rue Denis-de-Rougemont 3",
+            "localite": "2108 Couvet"
+        },
+        {
+            "nom": "OBRIST",
+            "prenom": "Laurent",
+            "adresse": "Rue de l'Hôpital 11",
+            "localite": "2001 Neuchâtel"
+        }
+    ]
