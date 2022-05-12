40ac0bd918c2dbcff059435d1dce031043c54cdc
leonardfrot <leonard.frote@he-arc.ch>
2022-05-12 11:00:08 +0200

    signup marche

--- authentification/index.js
+++ authentification/index.js
@@ -1,18 +1,33 @@
 const express = require('express')
 const app = express()
-const users = require('./user.json')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const User = require('./model/User')
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.8mjfx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // Middleware qui permet de lire les réponse POST
 
 app.use(express.json())
 
-app.get('/api/user', (req, res)=>{
-  res.status(200).json(users)
-})
-
-app.post('/api/user', (req, res)=>{
+app.post('/api/signup', (req, res)=>{
   console.log(req.body)
-  res.status(200).json(users)
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        login: req.body.login,
+        password: hash
+      });
+
+      user.save()
+      .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+      .catch(error => res.status(400).json({ error }));
+  })
+  .catch(error => res.status(500).json({ error }));
 })
 
 
